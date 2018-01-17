e79eb267f56d00737b5ca6db00473a4158c9b6cf
Antoni Kepinski <keppi@o2.pl>
2018-01-17 21:41:17 +0100

    Update 1.0.4 - Added tests!

---
+++ test.js
@@ -0,0 +1,12 @@
+import test from 'ava';
+import execa from 'execa';
+
+test('Test --version flag.', async t => {
+	const {stdout} = await execa('./cli.js', ['--version']);
+	t.true(stdout.length === 0);
+});
+
+test('Test general output with --today flag.', async t => {
+	const {stdout} = await execa('./cli.js', ['--today']);
+	t.true(stdout.length === 69);
+});
