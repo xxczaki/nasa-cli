80fe472f00b0a44e2a2a0dc02c448381f18c30d9
Antoni Kepinski <keppi@o2.pl>
2018-01-24 21:08:24 +0100

    Add files via upload

--- test.js
+++ test.js
@@ -8,5 +8,10 @@ test('Test --version flag.', async t => {
 
 test('Test general output with --today flag.', async t => {
 	const {stdout} = await execa('./cli.js', ['--today']);
-	t.true(stdout.length > 10);
+	t.true(stdout.length > 5);
+});
+
+test('Test general output with --date flag.', async t => {
+	const {stdout} = await execa('./cli.js', ['--date', '171224']);
+	t.true(stdout.length > 5);
 });
